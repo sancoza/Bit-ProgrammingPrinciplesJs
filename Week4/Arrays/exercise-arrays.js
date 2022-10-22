81b3e6b6a8dd35c89557302c5646d410cb60e31e
Sandra <sandrabalog@rocketmail.com>
2022-10-22 13:40:18 +0200

    finish exercise arrays and switch

--- Week4/Arrays/exercise-arrays.js
+++ Week4/Arrays/exercise-arrays.js
@@ -2,9 +2,7 @@
 
 // var months = ["January", "February","March","April","May", "June","July","August","September", "October","Noveber","December"];
 
-
 // console.log(months[5], months[9], months[0]);
-
 
 // // exercise 2
 
@@ -17,7 +15,6 @@
 // var numbers = [2, -4, 5, -2, -11];
 
 // console.log(numbers[1],numbers[3],numbers[4]);
-
 
 // exercise 4
 
@@ -33,7 +30,7 @@
 // console.log(arr[7] % 3 === 0); //false
 // console.log(arr[8] % 3 === 0); //true
 
-// // exercise 5
+// exercise 5
 
 // var array = [
 //   [1, 2, 1, 24],
@@ -42,3 +39,5 @@
 //   [7, 4, 28, 14],
 //   [3, 10, 26, 7],
 // ];
+
+// console.log(array[3][2]);
