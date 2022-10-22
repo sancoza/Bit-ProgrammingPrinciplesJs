a06605f4c1e8e81ce442dd2d0131d7e995dd3f9a
Sandra <sandrabalog@rocketmail.com>
2022-10-22 13:03:28 +0200

    exercise

---
+++ Week4/Arrays/index.js
@@ -0,0 +1,44 @@
+// // exercise 1
+
+// var months = ["January", "February","March","April","May", "June","July","August","September", "October","Noveber","December"];
+
+
+// console.log(months[5], months[9], months[0]);
+
+
+// // exercise 2
+
+// var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+
+// console.log(day[6]);
+
+// // exercise 3
+
+// var numbers = [2, -4, 5, -2, -11];
+
+// console.log(numbers[1],numbers[3],numbers[4]);
+
+
+// exercise 4
+
+// var arr = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// console.log(arr[0] % 3 === 0); //false
+// console.log(arr[1] % 3 === 0); //true
+// console.log(arr[2] % 3 === 0); //false
+// console.log(arr[3] % 3 === 0); //false
+// console.log(arr[4] % 3 === 0); //true
+// console.log(arr[5] % 3 === 0); //true
+// console.log(arr[6] % 3 === 0); //true
+// console.log(arr[7] % 3 === 0); //false
+// console.log(arr[8] % 3 === 0); //true
+
+// // exercise 5
+
+// var array = [
+//   [1, 2, 1, 24],
+//   [8, 11, 9, 4],
+//   [7, 0, 7, 27],
+//   [7, 4, 28, 14],
+//   [3, 10, 26, 7],
+// ];
---
+++ Week4/Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+var arr = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// console.log(arr[0] % 3 === 0); //false
+// console.log(arr[1] % 3 === 0); //true
+// console.log(arr[2] % 3 === 0); //false
+// console.log(arr[3] % 3 === 0); //false
+// console.log(arr[4] % 3 === 0); //true
+// console.log(arr[5] % 3 === 0); //true
+// console.log(arr[6] % 3 === 0); //true
+// console.log(arr[7] % 3 === 0); //false
+// console.log(arr[8] % 3 === 0); //true
+
+// // exercise 5
+
+// var array = [
+//   [1, 2, 1, 24],
+//   [8, 11, 9, 4],
+//   [7, 0, 7, 27],
+//   [7, 4, 28, 14],
+//   [3, 10, 26, 7],
+// ];
