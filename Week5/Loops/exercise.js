188be14a236036d58bd633f54d78b5591287b81f
Sandra <sandrabalog@rocketmail.com>
2022-10-30 22:17:02 +0100

    loop

--- Week5/Loops/exercise.js
+++ Week5/Loops/exercise.js
@@ -1,22 +1,21 @@
 // 1.Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
 // number is odd or even, and display a message to the screen.
 
-// for ( var x =0; x<15; x++) {
-//   if(x === 0) {
-//     console.log(x + "is even");
-//   }else if ( x % 2 === 0) {
-// console.log(x + "is even");
+// for (var i = 0; i < 15; i++ ) {
+//   if ( i % 2 === 0) {
+//     console.log(i + "is even");
 //   }else {
-//     console.log(x + "is odd");
+//     console.log(i + "odd");
 //   }
 // }
+// var sum =0;
 
 // 2. Write a program to sum the multiples of 3 and 5 under 1000.
 
-// var sum = 0;
+// var sum =0;
 
-// for (var i = 0; i < 1000; i++) {
-//   if (i % 3 === 0 || i % 5 === 0) {
+// for ( var i = 0; i < 1000; i++) {
+//   if (i % 3 === 0 && i % 5 === 0 ) {
 //     sum += i;
 //   }
 // }
@@ -25,23 +24,24 @@
 // 3.Write a program to compute the sum and product of an array of integers.
 
 // var array = [1,2,3,4,5,6];
-// sum=0;
-// product=1;
+
+// var sum = 0;
+// var product = 1;
+
 // for ( i = 0; i < array.length; i++) {
-// sum += array[i];//1+2+3+4+5+6
-// product *= array[i];//1*2*3*4*5*6
+//   sum += array[i];
+//   product *= array[i];
 // }
-// console.log("Sabiranje: "+ sum + " Množenje " + product);
+// console.log(sum,product);
 
 // 4. Write a program which prints the elements of the following array as a single string.
 
 // var x = ['1','A','B','c','r', true, NaN, undefined];
-// var element="";
-
-// for (i =0; i < x.length; i++) {
-//   element += " " + x[i];
+// var el="";
+// for (var i = 0; i < x.length; i++) {
+//    el += " " + x[i];
 // }
-// console.log(element);
+// console.log(el);
 
 // 5. Write a program that prints the elements of the following array.
 
@@ -51,14 +51,15 @@
 //   [7, 0, 7, 27]
 //   ];
 
-// var element="";
+//   var el ="";
 
 //   for (i = 0; i < a.length; i++) {
-// for(var j=0; j < a[i].length; j++){
-//   element += a [i][j] + " ";
-// }
+
+//   for ( j = 0; j < a[i].length; j++) {
+//    el += " " + (a[i][j]);
 //   }
-//   console.log(element);
+//   }
+//   console.log(el);
 
 // 6. Write a program that outputs the sum of squares of the first 20 numbers.
 
@@ -72,34 +73,37 @@
 // 7. Write a program that computes average marks of the following students. Then use this
 // average to determine the corresponding grade.
 
-// var students = [
-//   ["David", 80],
-//   ["Marko", 70],
-//   ["Dany", 88],
-//   ["John", 95],
-//   ["Thomas", 68]
-// ];
+// var sum=0;
 
-// var avggrade = 0;
-// for (var i = 0; i < students.length; i++){
-//     avggrade += students [i][1];
-//     var avg = (avggrade/students.length);
+// for (let i = 0; i <= 20; i++) {
+//   sum += i*i;
 // }
-// console.log("Average grade: " + avg);
-// if (avg < 60){
-//     console.log("Grade: F");
+// console.log(sum);
+
+// var array = [
+//   ['David',80],
+//   ['Marko', 77],
+//   ['Dany',88],
+//   ['John',95],
+//   ['Thomas', 68]
+// ];
+// var ocena =0;
+
+// for ( i = 0; i < array.length; i++) {
+//   ocena += array[i][1];
 // }
-// else if (avg < 70){
-//     console.log("Grade: D");
-// }
-// else if (avg < 80){
-// console.log("Grade: C");
-// }
-// else if (avg < 90){
-//     console.log("Grade: B");
-// }
-// else if (avg < 100){
-//     console.log("Grade: A");
+// var avg= (ocena/array.length);
+
+// if(avg < 60) {
+//   console.log("F");
+// }else if (avg < 70) {
+//   console.log("D");
+// }else if ( avg < 80) {
+//   console.log("C");
+// }else if ( avg < 90) {
+//   console.log("B");
+// }else if (avg < 100) {
+//   console.log("A");
 // }
 // 8. Write a program that uses console.log to print all the numbers from 1 to 100, with two
 // exceptions. For numbers divisible by 3, print Fizz; instead of the number, and for numbers
@@ -107,14 +111,18 @@
 // program to print FizzBuzz, for numbers that are divisible by both 3 and 5 (and still print
 // Fizz or Buzz for numbers divisible by only one of those).
 
-// for (i = 1; i < 10; i++) {
-//   if ( i % 3 === 0 && i % 5 === 0) {
-//     console.log("Fizz Buzz");
-//   }else if (i % 3 === 0) {
-//     console.log("Fizz");
-//   }else if ( i % 5 === 0) {
-//     console.log("Buzz");
-//   }else {
-//     console.log(i);
+// for (i = 0; i < 20; i++) {
+//   console.log(i);
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('Fizz buzz');
+//     continue;
+//   }
+//   if (i % 3 === 0) {
+//     console.log('Fizz');
+//     continue;
+//   }
+//   if (i % 5 === 0) {
+//     console.log('Buzz');
+//     continue;
 //   }
 // }
--- Week5/Loops/exercise2.js
+++ Week5/Loops/exercise2.js
@@ -5,17 +5,21 @@
 // Output: no
 
 // var e = 3;
-// var a = [5, -4.2, 3, 7];
 
+// var a =[5, -4.2, 3, 7];
+// var found=false;
 
-// for (i=0; i < a.length; i++) {
-//     if (e === a[i]) {
-//         console.log("Yes");
-//         break;
-//     } else {
-//         console.log("No");
-//         break;
-//     }
+// for (i = 0; i < a.length; i++) {
+//   if (e === a[i]) {
+//     found=true;
+//   }
+// }
+
+// if (found) {
+//   console.log("yes");
+// }
+// else{
+//   console.log("no");
 // }
 
 // 2. Write a program that multiplies every positive element of a given array by 2.
@@ -23,9 +27,10 @@
 // Output array: [-3, 22, 10, 6.8, -8]
 
 // var array = [-3, 11, 5, 3.4, -8];
-// for (var i = 0; i < array.length; i++) {
+
+// for (i = 0; i < array.length; i++ ) {
 //   if (array[i] > 0) {
-//     array[i] = array[i] * 2;
+//     array[i]= array[i]*2;
 //   }
 // }
 // console.log(array);
